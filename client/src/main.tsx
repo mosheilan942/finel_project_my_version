851b73e2b2ea958a02b0ed38df7781cfbe085ff8
ChaimKleinerman <135823187+ChaimKleinerman@users.noreply.github.com>
2023-12-04 13:51:53 +0200

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,7 @@ import Router from './routes/router.tsx'
 import UserContextProvider from './UserContext.tsx'
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
-// if (import.meta.env.VITE_NODE_ENV === "production") disableReactDevTools()
+if (import.meta.env.VITE_NODE_ENV === "production") disableReactDevTools()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
